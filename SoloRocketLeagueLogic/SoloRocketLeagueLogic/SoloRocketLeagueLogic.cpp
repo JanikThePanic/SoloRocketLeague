1e176ba2c509014ca0e7edf4bb2bfa87968e831a
Styxies <naniisamod@gmail.com>
2020-05-24 05:39:26 -0400

    Adding the DLL Boiler plate

---
+++ SoloRocketLeagueLogic/SoloRocketLeagueLogic/SoloRocketLeagueLogic.cpp
@@ -0,0 +1,16 @@
+// SoloRocketLeagueLogic.cpp : Defines the exported functions for the DLL.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "SoloRocketLeagueLogic.h"
+
+
+// This is an example of an exported variable
+SOLOROCKETLEAGUELOGIC_API int nSoloRocketLeagueLogic=0;
+
+// This is an example of an exported function.
+SOLOROCKETLEAGUELOGIC_API int fnSoloRocketLeagueLogic(void)
+{
+    return 0;
+}
