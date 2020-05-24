50e60a25de81d3267d058cc25bba1f0c243f7ef0
Styxies <naniisamod@gmail.com>
2020-05-24 09:56:04 -0400

    Camera work

---
+++ SoloRocketLeague/Assets/Classes/Camera/CameraCollision.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollision : MonoBehaviour
+{
+    public float MaxDistance = 5f;
+    public float Smoothing = 10f;
+    private Vector3 Direction;
+    private float Distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Direction = transform.localPosition.normalized;
+        Distance = Direction.magnitude;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 desiredCameraPosition = transform.parent.TransformPoint(Direction * MaxDistance);
+        RaycastHit hitObject;
+        if(Physics.Linecast(transform.parent.position, desiredCameraPosition, out hitObject))
+        {
+            Distance = Mathf.Clamp((hitObject.distance*0.85f), 3.0f, MaxDistance);
+        }
+        else
+        {
+            Distance = MaxDistance;
+        };
+
+        transform.localPosition = Vector3.Lerp(transform.localPosition, Direction * Distance, Time.deltaTime * Smoothing);
+    }
+}
---
+++ SoloRocketLeague/Assets/Classes/Camera/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float MouseSensitivity = 200f;
+    public float CameraMoveSpeed = 100f;
+    public GameObject TheCamera;
+    public GameObject ToFollow;
+    private float rotX;
+    private float rotY;
+    private float maxAngle = 80f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotX = 0f;
+        rotY = 0f;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMovement = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+        float yMovement = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+        rotX += xMovement;
+        rotY -= yMovement;
+        transform.rotation = Quaternion.Euler(Mathf.Clamp(rotY, -maxAngle, maxAngle), rotX, 0f);
+    }
+
+    void LateUpdate()
+    {
+        float step = CameraMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, ToFollow.transform.position, step);
+    }
+
+}
